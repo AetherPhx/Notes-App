1efd6ae7a579c53d82e1ae319d3bf9b19456f089
AetherPhx <aetherphx@gmail.com>
2024-12-15 19:04:01 -0500

    HotFix TS no reconocía las rutas absolutas

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,56 @@ export default defineConfig({
 				find: "@styles",
 				replacement: path.resolve(path.join(__dirname, "/src/styles")),
 			},
+			{
+				find: "@assets",
+				replacement: path.resolve(path.join(__dirname, "/src/assets")),
+			},
+			{
+				find: "@context",
+				replacement: path.resolve(path.join(__dirname, "/src/context")),
+			},
+			{
+				find: "@models",
+				replacement: path.resolve(path.join(__dirname, "/src/models")),
+			},
+			{
+				find: "@storage",
+				replacement: path.resolve(path.join(__dirname, "/src/storage")),
+			},
+			{
+				find: "@components",
+				replacement: path.resolve(path.join(__dirname, "/src/components")),
+			},
+			{
+				find: "@atoms",
+				replacement: path.resolve(
+					path.join(__dirname, "/src/components/atoms")
+				),
+			},
+			{
+				find: "@molecules",
+				replacement: path.resolve(
+					path.join(__dirname, "/src/components/molecules")
+				),
+			},
+			{
+				find: "@organisms",
+				replacement: path.resolve(
+					path.join(__dirname, "/src/components/organisms")
+				),
+			},
+			{
+				find: "@pages",
+				replacement: path.resolve(
+					path.join(__dirname, "/src/components/pages")
+				),
+			},
+			{
+				find: "@templates",
+				replacement: path.resolve(
+					path.join(__dirname, "/src/components/templates")
+				),
+			},
 		],
 	},
 });
