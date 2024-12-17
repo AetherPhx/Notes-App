6f892053035f339eea314d5e476930325f4c45be
AetherPhx <aetherphx@gmail.com>
2024-12-17 13:58:46 -0500

    Agregar notas predeterminadas y props a los metodos crud del context

--- src/context/NotesContext.tsx
+++ src/context/NotesContext.tsx
@@ -40,6 +40,35 @@ export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
 	const [errorMessage, setErrorMessage] = useState<string | null>(null);
 	const [noteList, setNoteList] = useState<Note[]>([]);
 	const [isEmpty, setIsEmpty] = useState(false);
+	const defaultNoteList: Note[] = [
+		new Note(
+			"Bienvenido! 👋🏽",
+			[
+				"📝 Esta app te permite crear y gestionar notas de manera rápida y sencilla.",
+				"Agrega notas rápidas, usalo para expresarte, escribe recordatorios, o simplemente toma notas. ¡Puedes hacer todo eso y más con esta app! 😊",
+			],
+			"yellow"
+		),
+		new Note(
+			"Llamar a mamá 📱",
+			[
+				"Mamá regresa de su viaje el viernes, no podré visitarla por lo que quiero llamarla para charlar un poco 😊",
+			],
+			"blue"
+		),
+		new Note(
+			"❓ Tutorial",
+			[
+				"Esta nota tiene mucho contenido por lo que en la sticky note solo se verá la primera parte. Sin embargo, una vez que abras la nota podrás acceder al contenido completo de la nota.",
+				"¡Dale click para poder visualizarlo por completo!",
+				"🎓 ¡Guia / Tutorial de la App! 🎓",
+				"Usa el formulario de la parte superior para agregar notas. Solo debes escribir el título y el contenido de la nota. Si lo deseas puedes cambiar el color de la nota.",
+				"Puedes eliminar fácilmente una nota solo dandole click al botón de eliminar desde las sticky notes.",
+				"Edita las notas dando click al botón de editar desde las sticky notes. Esto cambiará el formulario al modo de edición donde puedes realizar los cambios que gustes.",
+			],
+			"red"
+		),
+	];
 
 	// Cargar notas al inicio de la app desde el storage
 	useEffect(() => {
@@ -49,12 +78,12 @@ export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
 			setErrorMessage(null);
 
 			// TODO: Agregar carga de notas desde el storage
-			const localNoteList: Note[] = [];
-			if (localNoteList) {
-				if (localNoteList.length > 0) setNoteList(localNoteList);
-				else setIsEmpty(true);
-			}
-			// TODO: Agregar notas por defecto de respaldo en caso de que no existan notas locales
+			// const localNoteList: Note[] = [];
+			// if (localNoteList) {
+			// 	if (localNoteList.length > 0) setNoteList(localNoteList);
+			// 	else setIsEmpty(true);
+			// }
+			setNoteList(defaultNoteList);
 		} catch (error) {
 			setHasError(true);
 			if (error instanceof Error) setErrorMessage(error.message);
@@ -64,10 +93,16 @@ export const NotesProvider = ({ children }: { children: React.ReactNode }) => {
 	}, []);
 
 	// * Funciones CRUD
-	const addNote = () => console.log("addNote");
-	const getNote = () => console.log("getNote");
-	const updateNote = () => console.log("updateNote");
-	const deleteNote = () => console.log("deleteNote");
+	const addNote = (title: string, content: string, color: string) =>
+		console.log("addNote:", { title, content, color });
+	const getNote = (id: string) => console.log("getNote:", id);
+	const updateNote = (
+		id: string,
+		title: string,
+		content: string,
+		color: string
+	) => console.log("Note updated:", { id, title, content, color });
+	const deleteNote = (id: string) => console.log("deleteNote:", id);
 
 	return (
 		<NotesContext.Provider
